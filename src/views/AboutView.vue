73c1a78820771bc35e2404803890075005d69ab2
Trevor Corcoran <SuperShadowPlay@gmail.com>
2024-01-10 15:39:23 -0500

    Updated About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="content">
-    <p>This is the about page. More information to come.</p>
+    <div class="header">
+      <p>
+        <b>Roll for XYZ</b> is free online initiative tracker designed with DnD 5e in mind
+      </p>
+      <br/>
+    </div>
 
-    <div class="button">
-      <v-btn variant="tonal" @click="openGitHubPage">
-        GitHub
-      </v-btn>
+    <div class="body">
+      <h2>How to create an encounter:</h2>
+      <p>
+        To get started, press the <v-icon icon="mdi-plus-circle-outline"/> button to add a creature to the initiative.
+        Once you are ready to begin, press the <v-icon icon="mdi-arrow-down-thick"/> to begin moving through the initiative.
+        The page will scroll to the creature who is currently taking their turn. When you are ready to move to the next, click the
+        <v-icon icon="mdi-arrow-down-thick"/> again. The page will scroll in an infinite loop, so even those 10 round encounters will work just fine!
+      </p>
+
+      <br/>
+
+      <h2>Source code:</h2>
+      <div class="button">
+        <v-btn variant="tonal" size="large" @click="openGitHubPage">
+          GitHub
+        </v-btn>
+      </div>
     </div>
   </div>
 </template>
@@ -19,10 +37,24 @@
 <style scoped>
 .content {
   margin: 15px;
+}
+
+.header {
+  font-size: 1.5em;
   text-align: center;
 }
 
+.body {
+  margin-right: 15px;
+  font-size: 1.1em;
+}
+
+.body > p {
+  margin-left: 15px;
+}
+
 .button {
-  margin: 15px;
+  margin-top: 5px;
+  margin-left: 15px;
 }
 </style>
