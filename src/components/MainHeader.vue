90cfc9bb1cc4d3635283df41ee01f16bb8af2bd9
Trevor Corcoran <SuperShadowPlay@gmail.com>
2024-01-10 14:55:28 -0500

    Fixed MainHeader styling

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="topbar">
-    <v-row>
-      <v-col>
-        <div class="left">
-          <h1>Roll for
-            <img class="logo" :src="logoImage" alt="X Y Z"/>
-          </h1>
-        </div>
-      </v-col>
 
-      <v-col cols="2">
-        <div class="right">
-          <router-link to="/about">
-            <v-btn variant="tonal">
-              About
-            </v-btn>
-          </router-link>
-        </div>
+    <div class="left">
+      <router-link to="/">
+        <img class="logo" :src="logoImage" alt="Website Logo"/>
+      </router-link>
+    </div>
 
-        <div class="right">
-          <router-link to="/">
-            <v-btn variant="tonal">
-              Home
-            </v-btn>
-          </router-link>
-        </div>
-      </v-col>
-    </v-row>
+
+    <div class="right">
+      <div class="button">
+        <router-link to="/about">
+          <v-btn variant="tonal">
+            About
+          </v-btn>
+        </router-link>
+      </div>
+
+      <div class="button">
+        <router-link to="/">
+          <v-btn variant="tonal">
+            Home
+          </v-btn>
+        </router-link>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -39,19 +38,42 @@
     margin-top: 10px;
     margin-left: 20px;
     margin-right: 20px;
+
+    width: auto;
+    max-width: 100%;
+
+    min-height: 50px;
+    max-height: 5vh;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
+
+  .left {
+    order: 1;
+    flex-shrink: 2;
+
+    max-width: 40%;
   }
 
   .right {
+    order: 2;
+    flex-grow: 2;
+    align-self: center;
+  }
+
+  .button {
     float: right;
-    margin: 5px;
+    margin-left: 10px;
   }
 
   .img {
     max-width: auto;
-    max-height: 100%;
+    max-height: auto;
   }
 
   .logo {
-    height: 50px;
+    height: 100%;
   }
 </style>
